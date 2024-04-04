4ad0f2fe39d32ee8dff23fa83fa1ab951e7df864
relcnob <f.relcnob@gmail.com>
2024-04-04 21:29:20 +0200

    cors fix

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,7 +6,7 @@ import * as cookieParser from 'cookie-parser';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
-  app.enableCors({origin: ["localhost:3000", "https://ola-5-comwell.vercel.app/"], credentials: true, allowedHeaders: 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept, Observe, authorization',});
+  app.enableCors({origin: ["localhost:3000", "https://ola-5-comwell.vercel.app"], credentials: true, allowedHeaders: 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept, Observe, authorization',});
   app.use(cookieParser());
   await app.listen(5000);
   console.log('Server is now live.');
